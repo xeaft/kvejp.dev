c8e558774e5a31a30288f6a18d888d2bc09f2407
xeaft <157919165+xeaft@users.noreply.github.com>
2024-03-29 15:05:32 +0100

    web update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let title = document.getElementsByTagName("h1")[0];
+let title = document.getElementById("titletext");
 let titleText = title.innerText;
 let titleHTML = title.innerHTML;
 let letters = "abcdefghijklmnopqrstuvwxyz";
@@ -13,20 +13,20 @@ let interval = setInterval(() => {
             clearInterval(letterInterval);
             title.innerHTML = titleHTML;
         } else {
-            newLetter = letters.charAt(Math.round(Math.random() * 100) % letters.length)
+            newLetter = letters.charAt(Math.round(Math.random() * 100) % letters.length);
             let newText = titleHTML.substring(0, letterInd) + newLetter + titleHTML.substring(letterInd + 1);
             title.innerHTML = newText;
             counter++;
         }
-    }, 50)
-}, 2500)
+    }, 50);
+}, 2500);
 
 
 let varScale = 1;
 let varUse = "svh";
 addEventListener("resize", (event) => {
     let tabs = document.getElementById("tabs");
-    let header = document.getElementById("headerdiv")
+    let header = document.getElementById("headerdiv");
     let width = window.innerWidth;
     let height = window.innerHeight;
 
@@ -44,7 +44,84 @@ addEventListener("resize", (event) => {
     }
 
     root.style.setProperty("--height-scale", `${varScale}${varUse}`);
-    root.style.setProperty("--width-scale", `${varScale}${varUse}`)
+    root.style.setProperty("--width-scale", `${varScale}${varUse}`);
 });
 
-window.dispatchEvent(new Event("resize"))
+window.dispatchEvent(new Event("resize"));
+
+let mainTextObjs = document.getElementsByTagName("p");
+let mainText = "";
+let currentObjectIndex = 0;
+let currentTextIndex = 0;
+
+function textRenderFunc(objIndex, interval) {
+    let textObject = mainTextObjs[objIndex];
+
+    if (mainText === "") {
+        mainText = textObject.innerText;
+        textObject.style.innerText = "";
+        textObject.style.display = "block";
+
+    }
+
+    let minRandNum = 12;
+    let maxRandNum = 20;
+    let nextPartLen = Math.round((Math.random() * 100)) % (maxRandNum - minRandNum) + minRandNum;
+    let end = false;
+    let textIndex = currentTextIndex - objIndex * mainText.length;
+
+    if (textIndex + nextPartLen >= mainText.length) {
+        nextPartLen = mainText.length - textIndex;
+        clearInterval(interval);
+        end = true;
+    }
+    currentTextIndex += nextPartLen;
+    textIndex = currentTextIndex - objIndex * mainText.length;
+    textObject.innerText = mainText.substring(0, textIndex);
+    return end
+}
+
+function textRenderIntervalFunc() {
+    let ended = textRenderFunc(currentObjectIndex, textRenderInterval)
+    if (ended && currentObjectIndex < mainTextObjs.length - 1) {
+        currentObjectIndex += 1;
+        mainText = "";
+        textRenderInterval = setInterval(textRenderIntervalFunc, 120);
+    }
+}
+
+let textRenderInterval = setInterval(textRenderIntervalFunc, 120)
+
+
+let mouseTrail = document.getElementById("mousetrailer");
+let hoverableElements = document.getElementsByClassName("-hoverable");
+let hoverable2 = document.getElementsByClassName("-hoverable2");
+
+document.onmousemove = (ev) => {
+    mouseTrail.style.left = ev.clientX + "px";
+    mouseTrail.style.top = ev.clientY + "px";
+};
+
+function handleMouseHover(element, mode) {
+    if (!mode) {
+        element.addEventListener("mouseenter", () => {
+            mouseTrail.style.boxShadow = "0 0 20px 8px #0ff";
+        })
+    } else {
+        element.addEventListener("mouseenter", () => {
+            mouseTrail.style.boxShadow = "0 0 20px 8px #f00";
+        })
+    }
+
+    element.addEventListener("mouseleave", () => {
+        mouseTrail.style.boxShadow = "0 0 50px 10px #fff";
+    })
+}
+
+for (let element of hoverableElements) {
+    handleMouseHover(element)
+}
+
+for (let element of hoverable2) {
+    handleMouseHover(element, 1)
+}
